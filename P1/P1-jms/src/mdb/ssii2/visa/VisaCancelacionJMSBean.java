ca6bfcdac591f5942160fa3d71db2c24d2f4ff15
fabian.gutierrez <fabian.gutierrez@estudiante.uam.es>
2022-03-11 00:53:47 +0100

    Ejercicio 11 (pendiente de revisar. Depende del transaccional en el futuro)

--- P1/P1-jms/src/mdb/ssii2/visa/VisaCancelacionJMSBean.java
+++ P1/P1-jms/src/mdb/ssii2/visa/VisaCancelacionJMSBean.java
@@ -25,42 +25,107 @@ import java.util.logging.Logger;
  */
 @MessageDriven(mappedName = "jms/VisaPagosQueue")
 public class VisaCancelacionJMSBean extends DBTester implements MessageListener {
-  static final Logger logger = Logger.getLogger("VisaCancelacionJMSBean");
-  @Resource
-  private MessageDrivenContext mdc;
+    static final Logger logger = Logger.getLogger("VisaCancelacionJMSBean");
+    @Resource
+    private MessageDrivenContext mdc;
 
-  private static final String UPDATE_CANCELA_QRY = null;
-   // TODO : Definir UPDATE sobre la tabla pagos para poner
-   // codRespuesta a 999 dado un código de autorización
+    //private static final String UPDATE_CANCELA_QRY = null;
+    // TODO : Definir UPDATE sobre la tabla pagos para poner
+    // codRespuesta a 999 dado un código de autorización
+    private static final String UPDATE_CANCELA_QRY =
+        "update pagos " +
+        "set codRespuesta=999 " +
+        "where idAutorizacion=?";
+    private static final String SELECT_CODRESPUESTA_QRY =
+        "select codRespuesta " +
+        "from pagos "+
+        "where idAutorizacion=?";
+    private static final String UPDATE_TARJETA_QRY =
+        "update tarjeta " +
+        "set saldo=saldo+pago.importe " +
+        "from pago " +
+        "where pago.idAutorizacion=? and pago.numeroTarjeta=tarjeta.numeroTarjeta";
 
 
-  public VisaCancelacionJMSBean() {
-  }
+    public VisaCancelacionJMSBean() {
+    }
 
-  // TODO : Método onMessage de ejemplo
-  // Modificarlo para ejecutar el UPDATE definido más arriba,
-  // asignando el idAutorizacion a lo recibido por el mensaje
-  // Para ello conecte a la BD, prepareStatement() y ejecute correctamente
-  // la actualización
-  public void onMessage(Message inMessage) {
-      TextMessage msg = null;
+    // TODO : Método onMessage de ejemplo
+    // Modificarlo para ejecutar el UPDATE definido más arriba,
+    // asignando el idAutorizacion a lo recibido por el mensaje
+    // Para ello conecte a la BD, prepareStatement() y ejecute correctamente
+    // la actualización
+    public void onMessage(Message inMessage) {
+        TextMessage msg = null;
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int idAutorizacion = 0;
 
-      try {
-          if (inMessage instanceof TextMessage) {
-              msg = (TextMessage) inMessage;
-              logger.info("MESSAGE BEAN: Message received: " + msg.getText());
-          } else {
-              logger.warning(
-                      "Message of wrong type: "
-                      + inMessage.getClass().getName());
-          }
-      } catch (JMSException e) {
-          e.printStackTrace();
-          mdc.setRollbackOnly();
-      } catch (Throwable te) {
-          te.printStackTrace();
-      }
-  }
+        try {
+            if (inMessage instanceof TextMessage) {
+                msg = (TextMessage) inMessage;
+                logger.info("MESSAGE BEAN: Message received: " + msg.getText());
+
+                idAutorizacion = Integer.parseInt(msg.getText());
+                con = Connection.getConnection();
+                pstmt = con.prepareStatement(SELECT_CODRESPUESTA_QRY);
+                errorLog(SELECT_CODRESPUESTA_QRY);
+
+                pstmt.setInteger(1, idAutorizacion);
+                rs = pstmt.executeQuery();
+                pstmt.close();
+
+                int codRespuesta = rs.getString("codRespuesta");
+                rs.close();
+                if (codRespuesta.equals("000")) {
+                    // Cancela pago
+                    pstmt = con.prepareStatement(UPDATE_CANCELA_QRY);
+                    errorLog(UPDATE_CANCELA_QRY);
+                    pstmt.setInteger(1, idAutorizacion);
+
+                    if (pstmt.execute() || pstmt.getUpdateCount() =! 1) {
+                        throw new EJBException();
+                    }
+                    pstmt.close();
+                    pstmt = null;
+
+                    // Rectifica saldo
+                    pstmt = con.prepareStatement(UPDATE_TARJETA_QRY);
+                    errorLog(UPDATE_TARJETA_QRY);
+                    pstmt.setInteger(1, idAutorizacion);
+
+                    if (pstmt.execute() || pstmt.getUpdateCount() =! 1) {
+                        throw new EJBException();
+                    }
+                    pstmt.close();
+                    pstmt = null;
+                }
+            } else {
+                logger.warning(
+                        "Message of wrong type: "
+                        + inMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+            mdc.setRollbackOnly();
+        } catch (Throwable te) {
+            te.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close(); rs = null;
+                }
+                if (pstmt != null) {
+                    pstmt.close(); pstmt = null;
+                }
+                if (con != null) {
+                    closeConnection(con); con = null;
+                }
+            } catch (SQLException e) {
+            }
+        }
+    }
 
 
 }
