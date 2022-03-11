a426005aa3b67f0e6db8781c7d354917748a0f22
fabian.gutierrez <fabian.gutierrez@estudiante.uam.es>
2022-03-11 12:19:09 +0100

    Ejercicio 12 (pendiente de probar)

--- P1/P1-jms/src/clientjms/ssii2/VisaQueueMessageProducer.java
+++ P1/P1-jms/src/clientjms/ssii2/VisaQueueMessageProducer.java
@@ -17,40 +17,42 @@ public class VisaQueueMessageProducer {
     // TODO: Anotar los siguientes objetos para
     // conectar con la connection factory y con la cola
     // definidas en el enunciado
+    @Resource(mappedName = "jms/VisaConnectionFactory")
     private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/VisaPagosQueue")
     private static Queue queue;
 
     // Método de prueba
     public static void browseMessages(Session session)
     {
-      try
-      {
-        Enumeration messageEnumeration;
-        TextMessage textMessage;
-        QueueBrowser browser = session.createBrowser(queue);
-        messageEnumeration = browser.getEnumeration();
-        if (messageEnumeration != null)
+        try
         {
-          if (!messageEnumeration.hasMoreElements())
-          {
-            System.out.println("Cola de mensajes vacía!");
-          }
-          else
-          {
-            System.out.println("Mensajes en cola:");
-            while (messageEnumeration.hasMoreElements())
+            Enumeration messageEnumeration;
+            TextMessage textMessage;
+            QueueBrowser browser = session.createBrowser(queue);
+            messageEnumeration = browser.getEnumeration();
+            if (messageEnumeration != null)
             {
-              textMessage =
-                (TextMessage) messageEnumeration.nextElement();
-              System.out.println(textMessage.getText());
+                if (!messageEnumeration.hasMoreElements())
+                {
+                    System.out.println("Cola de mensajes vacía!");
+                }
+                else
+                {
+                    System.out.println("Mensajes en cola:");
+                    while (messageEnumeration.hasMoreElements())
+                    {
+                        textMessage =
+                            (TextMessage) messageEnumeration.nextElement();
+                        System.out.println(textMessage.getText());
+                    }
+                }
             }
-          }
         }
-      }
-      catch (Exception e)
-      {
-        e.printStackTrace();
-      }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -60,21 +62,26 @@ public class VisaQueueMessageProducer {
         TextMessage message = null;
 
         if (args.length != 1) {
-          System.err.println("Uso: VisaQueueMessageProducer [-browse | <msg>]");
-          return;
+            System.err.println("Uso: VisaQueueMessageProducer [-browse | <msg>]");
+            return;
         }
 
         try {
-          // TODO: Inicializar connectionFactory
-          // y queue mediante JNDI
+            // TODO: Inicializar connectionFactory
+            // y queue mediante JNDI
+            //
+            // InitialContext jndi = new InitialContext();
+            // connectionFactory =
+            // (ConnectionFactory)jndi.lookup("jms/VisaConnectionFactory");
+            // queue = (Queue)jndi.lookup("jms/VisaPagosQueue");
 
-          connection = connectionFactory.createConnection();
-          session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-          if (args[0].equals("-browse")) {
-            browseMessages(session); 
-          } else {
-            // TODO: Enviar argv[0] como mensaje de texto
-          }
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            if (args[0].equals("-browse")) {
+                browseMessages(session); 
+            } else {
+                // TODO: Enviar argv[0] como mensaje de texto
+            }
         } catch (Exception e) {
             System.out.println("Excepcion : " + e.toString());
         } finally {
